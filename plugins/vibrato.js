bf3d6b7ced0b61bd368b61b0189d200061fe759c
Devan Wolf <8494722+DevanWolf@users.noreply.github.com>
2021-01-25 11:17:39 -0600

    Improved and simplified command

--- plugins/vibrato.js
+++ plugins/vibrato.js
@@ -7,10 +7,7 @@ module.exports = {
 			fs.unlinkSync(temp);
 		if (fs.existsSync(video))
 			fs.renameSync(video,temp);
-		var command = " -i \"" + temp + "\""
-				+ " -ar 44100"
-				+ " -vf scale="+toolbox.width+"x"+toolbox.height+",setsar=1:1,fps=fps="+toolbox.fps
-				+ " -af vibrato=f=7.0:d=0.5 -y \"" + video + "\"";
+		var command = " -i \"" + temp + "\" -c:v copy -af vibrato=f=7 -y \"" + video + "\"";
 		global.ffmpeg.runSync(command + (debug == false ? " -hide_banner -loglevel quiet" : ""));
 		fs.unlinkSync(temp);
 		return true
