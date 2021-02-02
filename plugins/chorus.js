56a68f70b4d281d091a8178386920ef6a4cc872f
Devan Wolf <8494722+DevanWolf@users.noreply.github.com>
2021-02-01 21:15:46 -0600

    Improved and simplified command

--- plugins/chorus.js
+++ plugins/chorus.js
@@ -7,7 +7,7 @@ module.exports = {
 			fs.unlinkSync(temp);
 		if (fs.existsSync(video))
 			fs.renameSync(video,temp);
-		var command = " -i \"" + temp + "\" -c:v copy -af chorus=\".5:.9:50|60|40:.4|.32|.3:.25|.4|.3:2|2.3|1.3\" -y \"" + video + "\"";
+		var command = "-i \"" + temp + "\" -c:v copy -af chorus=\".5:.9:50|60|40:.4|.32|.3:.25|.4|.3:2|2.3|1.3\" -y \"" + video + "\"";
 		global.ffmpeg.runSync(command + (debug == false ? " -hide_banner -loglevel quiet" : ""));
 		fs.unlinkSync(temp);
 		return true
