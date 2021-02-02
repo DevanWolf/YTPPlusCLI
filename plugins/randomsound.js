6567d95b7f2b4601e08bb19e80956da4e6adbdee
Devan Wolf <8494722+DevanWolf@users.noreply.github.com>
2021-02-01 21:10:15 -0600

    Improved command; now faster using copy video

--- plugins/randomsound.js
+++ plugins/randomsound.js
@@ -19,7 +19,7 @@ module.exports = {
 			fs.unlinkSync(temp);
 		if (fs.existsSync(video))
 			fs.renameSync(video,temp);
-		var command = " -i \"" + cwd + "/shared/temp/temp.mp4\" -i \"" + soundDir + "/" + randomSound + "\" -c:v copy -ar 44100 -ac 2 -filter_complex \"[0:a][1:a]amix,volume=2[out]\" -map 0:v -map [out] -shortest -map_metadata -1 -y \"" + video + "\"";
+		var command = "-i \"" + cwd + "/shared/temp/temp.mp4\" -i \"" + soundDir + "/" + randomSound + "\" -c:v copy -ar 44100 -ac 2 -filter_complex \"[0:a][1:a]amix,volume=2[out]\" -map 0:v -map [out] -shortest -map_metadata -1 -y \"" + video + "\"";
 		global.ffmpeg.runSync(command + (debug == false ? " -hide_banner -loglevel quiet" : ""));
 		fs.unlinkSync(temp);
 		return true
